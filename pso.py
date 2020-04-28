2694e2e16b028d2e3a7b41119e78b700119858f4
62random <josepedropinto98@gmail.com>
2020-04-28 23:08:56 +0100

    pso module

--- pso.py
+++ pso.py
@@ -1,10 +1,13 @@
 import random
 import matplotlib.pyplot as plt
 import sys
+import numpy as np
+import pandas as pd
 
 
 class ParticleSwarm:
-    def __init__(self, dimensions, size, chi=0.729, phi1=2.05, phi2=2.05):
+    def __init__(self, eval_function, dimensions, size, chi=0.729, phi1=2.05, phi2=2.05):
+        self.eval_function = eval_function
         self.dimensions = dimensions
         self.size = size
         self.chi = chi
@@ -14,13 +17,16 @@ class ParticleSwarm:
         self.best_position = []
         self.best_position_quality = sys.float_info.max
 
-    def run(self, max_iterations=100, stopping_rounds=10, render=True):
+    def run(self, max_iterations=100, stopping_rounds=10, stopping_tolerance=0.001, render=False, as_dataframe=False):
         positions = []
         qualities = []
         iteration = 0
         stop_round_counter = 0
+        if as_dataframe:
+            lines = []
         while iteration < max_iterations:
             for i in range(self.size):
+                self.particles[i].evaluate(self.eval_function)
                 if self.particles[i].position_quality < self.best_position_quality:
                     self.best_position = self.particles[i].position.copy()
                     self.best_position_quality = self.particles[i].position_quality
@@ -29,28 +35,39 @@ class ParticleSwarm:
                 self.particles[i].update_velocity()
                 self.particles[i].update_position()
 
+                if as_dataframe:
+                    lines.append(self.particles[i].position + [self.particles[i].position_quality, iteration])
+
             positions.append(self.best_position)
             qualities.append(self.best_position_quality)
 
-            if qualities[-1] == qualities[-2]:
-                stop_round_counter += 1
-            else:
-                stop_round_counter = 0
+            if iteration > 1:
+                if abs(qualities[-1] - qualities[-2])/qualities[-2] < stopping_tolerance:
+                    stop_round_counter += 1
+                else:
+                    stop_round_counter = 0
 
             if stop_round_counter >= stopping_rounds:
                 break
 
-            if render:
-                plt.plot(qualities)
+            iteration += 1
+        if render:
+            plt.plot(qualities)
 
-        return positions, qualities
+        if as_dataframe:
+            df = pd.DataFrame(np.array(lines),
+                              columns=[f"x_{i}" for i in range(len(self.dimensions))] + ['quality', 'iteration'])
+
+            return df, qualities, positions
+
+        return qualities, positions
 
 
 class Particle:
     def __init__(self, swarm):
         self.swarm = swarm
-        self.position = [random.uniform(x[0], x[1]) for x in swarm.dimensions]        # random starting position
-        self.velocity = [random.uniform(-1, 1) for _ in swarm.dimensions]             # random initial velocity
+        self.position = [random.uniform(x[0], x[1]) for x in swarm.dimensions]  # random starting position
+        self.velocity = [random.uniform(-1, 1) for _ in swarm.dimensions]       # random initial velocity
         self.best_position = []
         self.best_position_quality = sys.float_info.max
         self.position_quality = sys.float_info.max
